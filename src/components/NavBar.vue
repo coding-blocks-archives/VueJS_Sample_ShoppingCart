558c5818337c984ffd173ce35bc304a6b97c1f3c
Arnav Gupta <arnav@codingblocks.com>
2018-10-01 14:52:43 +0530

    navbar created
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar toggleable="md">
+    <b-navbar-brand href="#">
+      Shopping Cart
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item>
+          <router-link to="/catalog">Catalog</router-link>
+        </b-nav-item>
+        <b-nav-item>
+          <router-link to="/cart">Cart</router-link>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+
+  </b-navbar>
+</template>
+
